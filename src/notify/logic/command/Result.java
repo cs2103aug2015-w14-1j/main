d2c9c88ace1e115672b9d19b9f0751bb898d94d6
sihao <sihao.box@gmail.com>
2015-11-06 03:01:34 +0800

    Refactor Classes with Comments and Structure

--- src/notify/logic/command/Result.java
+++ src/notify/logic/command/Result.java
@@ -1,60 +1,90 @@
+/**
+ * Author: Chua Si Hao
+ * Matric No: A0125471L
+ * For CS2103T - Notify
+ */
+
 package notify.logic.command;
 
 import java.util.ArrayList;
 
 import notify.Task;
 
+/**
+ * The Result class stores the retrieve results to be used by 
+ * Command class and Logic class
+ */
 public class Result {
 
+	// These are variables used for quick access and default values required
 	private static final int FIRST_RESULT_INDEX = 0;
 	private static final boolean DEFAULT_IS_SUCCESS = true;
 
+	// These are variables used to indicate the status of the results
 	private Action actionPerformed;
 	private ArrayList<Task> results;
 	private boolean isSuccess;
 
 	public Result(Action actionPerformed, ArrayList<Task> results) {
+		
 		this(actionPerformed, results, DEFAULT_IS_SUCCESS);
+	
 	}
 
-	public Result(Action actionPerformed, ArrayList<Task> results,
-			boolean isSuccess) {
+	public Result(Action actionPerformed, ArrayList<Task> results, boolean isSuccess) {
+		
 		this.actionPerformed = actionPerformed;
 		this.results = results;
 		this.isSuccess = isSuccess;
-	}
-
-	public Action getActionPerformed() {
-		return actionPerformed;
-	}
-
 	
+	}
+
 	/**
-	 * Returns the first task in the ArrayList of results.
-	 * 
-	 * @return	task at index 0 in the ArrayList of results
+	 * Returns the Action type that is requested for
 	 */
+	public Action getActionPerformed() {
+		
+		return actionPerformed;
 	
+	}
+
+	/**
+	 * Returns the first item on the results list for quick access
+	 * 
+	 * @return Task containing the relevant results stored in within
+	 * 			if the results list is empty, null is returned
+	 * 
+	 */
 	public Task getFirstResult() {
 
 		if (results.isEmpty()) {
+			
 			return null;
+		
 		}
 
 		return results.get(FIRST_RESULT_INDEX);
+	
 	}
 
 	/**
-	 * Returns the ArrayList of tasks.
-	 * 
-	 * @return	ArrayList of tasks
+	 * Returns the list of results retrieved
 	 */
-	
 	public ArrayList<Task> getResults() {
+		
 		return results;
+	
 	}
 
+	/**
+	 * Returns the state of execution of the action performed
+	 * 
+	 * @return	true if the execution is successful
+	 * 
+	 */
 	public boolean isSuccess() {
+		
 		return isSuccess;
+	
 	}
 }
--- src/notify/logic/logger/LogType.java
+++ src/notify/logic/logger/LogType.java
@@ -1,5 +1,14 @@
+/**
+ * Author: Chua Si Hao
+ * Matric No: A0125471L
+ * For CS2103T - Notify
+ */
+
 package notify.logic.logger;
 
+/**
+ * This LogType enumeration indicates the log types the user can log with
+ */
 public enum LogType { 
 	
 	DEBUG, ERROR, INFO, VERBOSE, WEIRD, WTF 
