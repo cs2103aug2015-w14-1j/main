e7351f4fd9c50d7b3f1e9c6c362e3378747197f3
sadhikabilla <svjbilla@gmail.com>
2015-11-07 18:25:37 +0800

    adding the HelpCommand

---
+++ src/notify/logic/command/HelpCommand.java
@@ -0,0 +1,26 @@
+package notify.logic.command;
+
+import java.util.ArrayList;
+
+import notify.Task;
+import notify.logic.TaskManager;
+
+public class HelpCommand extends Command {
+	
+	TaskManager manager;
+
+	public HelpCommand(Action commandAction, TaskManager manager) {
+		
+		super(commandAction);
+		this.manager = manager;
+		
+	}
+
+	@Override
+	public Result execute() {
+		
+		Result result = new Result(Action.HELP, new ArrayList<Task>());
+		return result;
+	}
+
+}
