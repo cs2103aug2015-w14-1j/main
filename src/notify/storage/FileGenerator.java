0b838875aeafcd30b9f7eb8ca8812c6f21d4a2ae
joshuaaung <mj_kyawswa@yahoo.com>
2015-11-09 01:55:28 +0800

    modifies storage

--- src/notify/storage/FileGenerator.java
+++ src/notify/storage/FileGenerator.java
@@ -1,4 +1,4 @@
-/* @@author A0124072 */
+ 
 package notify.storage;
 
 import java.io.BufferedReader;
@@ -10,13 +10,9 @@ import java.io.IOException;
 
 import notify.logic.command.Action;
 
-/**
- * FileGenerator class is responsible for generating system folders: notify.config, data, commands
- * and generating system files: tasks(data) file, path file and 
- * various command(as of {@see notify.logic.command.Action}) files.
- */
+ 
 public class FileGenerator {
-	/** These variables represent the folders and files to be generated */
+	 
 	private File configFolder;
 	private File dataFolder;
 	private File commandsFolder;
@@ -24,12 +20,7 @@ public class FileGenerator {
 	private File commandFile;
 	private File dataFile;
 	
-	/** 
-	 * This class's Constructor which instantiate the files and folders that are to be generated.
-	 * It then invokes {@link #generateConfigFolder() generateConfigFolder} method to create all the
-	 * necessary folders. After which it invokes {@link #generateSystemFiles() generateSystemFiles}
-	 * to create necessary files.
-	 */
+	 
 	public FileGenerator() {
 		this.configFolder = new File(Constants.NAME_FOLDER_CONFIG);
 		this.dataFolder = new File(String.format(Constants.PATH_SUB_FOLDER, Constants.FOLDER_CONFIG,
@@ -40,58 +31,32 @@ public class FileGenerator {
 		generateSystemFiles();
 	}
 	
-	/** 
-	 * This method returns the File object which represents the notify.config folder 
-	 * 
-	 * @return the File object which represents the notify.config folder
-	 */
+	 
 	public File getConfigFolder() {
 		return this.configFolder;
 	}
 	
-	/** 
-	 * This method returns the File object which represents the data folder 
-	 * 
-	 * @return the File object which represents the data folder
-	 */
+	 
 	public File getDataFolder() {
 		return this.dataFolder;
 	}
 	
-	/** 
-	 * This method returns the File object which represents the commands folder 
-	 * 
-	 * @return the File object which represents the commands folder
-	 */
+	 
 	public File getCommandsFolder() {
 		return this.commandsFolder;
 	}
 	
-	/** 
-	 * This method returns the File object which represents the path file 
-	 * 
-	 * @return the File object which represents the path file
-	 */
+	 
 	public File getDirectoryFile() {
 		return this.directoryFile;
 	}
 	
-	/** 
-	 * This method returns the File object which represents the data file
-	 * 
-	 * @return the File object which represents the data file
-	 */
+	 
 	public File getDataFile() {
 		return this.dataFile;
 	}
 	
-	/** 
-	 * This method returns the File object which represents the specified command file
-	 * 
-	 * @param command_		the Action object which corresponds to the command file
-	 * 
-	 * @return the File object which represents the specified command file
-	 */
+	 
 	public File getCommandFile(Action command_) {
 		assert (command_ != null);
 		File file = new File(String.format(Constants.PATH_COMMAND_FILE, Constants.FOLDER_CONFIG,
@@ -100,14 +65,7 @@ public class FileGenerator {
 		return file;
 	}
 	
-	/** 
-	 * This method returns the contents of the specified command file as a String object
-	 * 
-	 * @param file_		the file to be read
-	 * 
-	 * @return the String object which represents the content of the first line in the 
-	 * 			specified file
-	 */
+	 
 	public String getFileContent(File file_) {
 		assert (file_.exists() == true);
 		String fileContent = readFromFile(file_);
@@ -115,19 +73,14 @@ public class FileGenerator {
 		return fileContent;
 	}
 
-	/** 
-	 * This method generates the system files: data file, path file and various commands files.
-	 * It invokes {@link #generateDirectoryFile() #generateDataFile() #generateCommandFiles()} methods
-	 */
+	 
 	private void generateSystemFiles() {
 		generateDirectoryFile();
 		generateDataFile();
 		generateCommandFiles();
 	}
 
-	/**
-	 * This method creates the system folders
-	 */
+	 
 	private void generateConfigFolder() {
 		if (!this.configFolder.exists()) {
 			this.configFolder.mkdir();
@@ -144,12 +97,7 @@ public class FileGenerator {
 		}
 	}
 
-	/**
-	 * This method generate path file which is located inside the data folder
-	 * If the path file does not exist, it creates new path file. 
-	 * It then invokes {@link #writeIntoFile(File, String)} method to write the 
-	 * default absolute path of the data file which is <tt>notify.config/data/</tt> into the path file
-	 */
+	 
 	private void generateDirectoryFile() {
 		assert (this.configFolder.exists() == true);
 		assert (this.dataFolder.exists() == true);
@@ -163,13 +111,7 @@ public class FileGenerator {
 		}
 	}
 	
-	/**
-	 * This method generate data file. It invokes {@link #readFromFile(File)} method to read the 
-	 * content of the path file and retrieve it which represents the absolute path of the data file.
-	 * It then creates a data file at the destination if it does not exist. 
-	 * It invokes {@link #writeIntoFile(File, String)} method to write 
-	 * {@value notify.storage.Constants#SQURE_BRACKETS} which represents the empty task list.
-	 */
+	 
 	private void generateDataFile() {
 		assert (this.configFolder.exists() == true);
 		assert (this.dataFolder.exists() == true);
@@ -196,9 +138,7 @@ public class FileGenerator {
 		}
 	}
 
-	/**
-	 * This method writes specified data into the specified file
-	 */
+	 
 	private void writeIntoFile(File file_, String dataToWrite_) {
 		FileWriter fileWriter;
 		BufferedWriter bufferedWriter;
@@ -218,10 +158,7 @@ public class FileGenerator {
 		}
 	}
 	
-	/**
-	 * This method reads from the specified file.
-	 * It then returns the content of the file.
-	 */
+	 
 	private String readFromFile(File file_) {
 		FileReader fileReader;
 		BufferedReader bufferedReader;
--- src/notify/storage/Storage.java
+++ src/notify/storage/Storage.java
@@ -1,4 +1,4 @@
-/* @@author A0124072 */
+ 
 package notify.storage;
 
 import java.util.ArrayList;
@@ -7,18 +7,9 @@ import java.util.HashMap;
 import notify.Task;
 import notify.logic.command.Action;
 
-/**
- * This Storage class is used to generate the system folders and files via FileGenerator class
- * {@see notify.storage.FileGenerator}, load and save the user's tasks/data via 
- * TasksLoader class{@see notify.storage.TasksLoader} and TasksSaver class{@see notify.storage.TasksSaver}.
- * It is also used to set the absolute file path of the data file via DataDirectoryManager class 
- * {@see notify.storage.DataDirectoryManager} and load the command variations 
- * via CommandsLoader class{@see notify.storage.CommandsLoader}
- */
+ 
 public class Storage {
-	/** 
-	 * These variables are used to interact with other internal classes of the storage component
-	 */
+	 
 	private FileGenerator fileGenerator;
 	private DataDirectoryManager dataDirectoryManager;
 	private TasksSaver save;
@@ -26,14 +17,7 @@ public class Storage {
 	private CommandsLoader loadCommand;
 	private String dataFileAbsolutePath;
 	
-	/**
-	 * This method is the class's Constructor which is used to instantiate the following variables
-	 * {@link #fileGenerator} {@link #dataDirectoryManager} {@link #save} {@link #load}
-	 * {@link #loadCommand} {@link #dataFileAbsolutePath}
-	 * 
-	 * It invokes {@see notify.storage.DataDirectoryManager#getDataFilePath()} to retrieve the 
-	 * currently saved absolute file path of the data file.
-	 */
+	 
 	public Storage() {
 		this.fileGenerator = new FileGenerator();
 		this.dataDirectoryManager = new DataDirectoryManager(String.format(Constants.PATH_FILE, Constants.FOLDER_CONFIG, Constants.FOLDER_DATA, Constants.FILE_DESTINATION));
@@ -43,43 +27,23 @@ public class Storage {
 		this.save = new TasksSaver(this.dataFileAbsolutePath);
 	}
 	
-	/**
-	 * This method loads the user's tasks/data from the data file
-	 * 
-	 * @return the ArrayList<Task> object which contains all the tasks/data stored in the data file
-	 */
+	 
 	public ArrayList<Task> loadTasks(){
 		return this.load.execute(new ArrayList<Task>());		
 	}
 
-	/**
-	 * This method saves the user's tasks/data into the data file
-	 * 
-	 * @param taskList_		the ArrayList<Task> object which contains the currently added tasks/data
-	 * 							which is to be written into the data file
-	 */
+	 
 	public void saveTasks(ArrayList<Task> taskList_) {
 		this.save.setFilePath(String.format(Constants.PATH_FILE, dataDirectoryManager.getDataFilePath(), Constants.FILE_DATA, Constants.EMPTY_STRING));
 		this.save.execute(taskList_);
 	}
 	
-	/**
-	 * This method loads the command variations from the command files
-	 * 
-	 * @return the HashMap<String, Action> object of which <Key> represents the various command variations
-	 * 			and <Value> represents the default system commands {@see notify.logic.command.Action}
-	 */
+	 
 	public HashMap<String, Action> loadCommands() {
 		return this.loadCommand.execute(new ArrayList<Task>());
 	}
 	
-	/**
-	 * This method sets the absolute file path where the data file has to be saved/moved to
-	 * 
-	 * @param newFilePath_		the String representation of the new absolute file path of the data file
-	 * 
-	 * @return 'true' if the specified absolute file path is a valid directory; 'false' otherwise
-	 */
+	 
 	public boolean setFileDestination(String newFilePath_) {
 		return this.dataDirectoryManager.execute(newFilePath_);
 	}
