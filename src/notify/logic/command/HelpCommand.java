b66d6e4ac419579af30850c48a9624b49f0d15c2
sadhikabilla <svjbilla@gmail.com>
2015-11-09 05:45:34 +0800

    adding comments

--- src/notify/logic/command/HelpCommand.java
+++ src/notify/logic/command/HelpCommand.java
@@ -7,8 +7,21 @@ import java.util.ArrayList;
 import notify.Task;
 import notify.logic.TaskManager;
 
+/**
+ * The HelpCommand class extends the abstract class ReversibleCommand.
+ * 
+ * The HelpCommand class is responsible for creating the Result object, corresponding
+ * to the HELP command.
+ * 
+ * This class contains the execute method called by Logic.
+ *
+ * @author sadhikabilla
+ *
+ */
+
 public class HelpCommand extends Command {
 	
+	//These are variables that are required to store the fields of each task 
 	TaskManager manager;
 
 	public HelpCommand(Action commandAction, TaskManager manager) {
@@ -18,6 +31,12 @@ public class HelpCommand extends Command {
 		
 	}
 
+	/**
+	 * This method is responsible for creating the Result object corresponding to the HELP action.
+	 * The result object is used by the Logic class.  
+	 * 
+	 * @return 'result' object corresponding to the HELP action. 
+	 */
 	@Override
 	public Result execute() {
 		
